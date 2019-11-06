7270e5e49ccb62d7c3b81ae7b1a69639acef9dcb
Mateusz Stadnik <matgla@live.com>
2019-11-06 06:16:30 +0100

    started working on dynamic linker

---
+++ scripts/generate_binary.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3 
+
+import os 
+import argparse 
+
+from elftools.elf.elffile import ELFFile
+from elftools.elf.sections import SymbolTableSection
+from elftools.elf.relocation import RelocationSection 
+from elftools.elf.descriptions import describe_reloc_type
+
+
+def get_symbols(elf_filename):
+    symbols = {}
+    with open(elf_filename, "rb") as elf_file:
+        elf = ELFFile(elf_file)
+        for section in elf.iter_sections():
+            if not isinstance(section, SymbolTableSection):
+                continue
+            for symbol in section.iter_symbols():
+                data = {}
+                data["type"] = symbol["st_info"]["type"]
+                data["binding"] = symbol["st_info"]["bind"]
+                data["name"] = symbol["st_name"]
+                data["value"] = symbol["st_value"]
+                data["size"] = symbol["st_size"]
+                data["section_index"] = symbol["st_shndx"]
+                data["visibility"] = symbol["st_other"]["visibility"]
+                symbols[symbol.name] = data
+    return symbols 
+
+def get_public_functions_from_symbols(symbols):
+    filtered_symbols = {}
+    for symbol_key in symbols:
+        symbol = symbols[symbol_key]
+        if symbol["type"] == "STT_FUNC":
+            if symbol["binding"] == "STB_GLOBAL" or symbol["binding"] == "STB_WEAK":
+                filtered_symbols[symbol_key] = symbol
+
+    return filtered_symbols
+
+def get_relocations(elf_filename):
+    relocations = [] 
+    with open(elf_filename, "rb") as elf_file:
+        elf = ELFFile(elf_file)
+        for section in elf.iter_sections():
+            if not isinstance(section, RelocationSection):
+                continue 
+            symbols = elf.get_section(section["sh_link"])
+            for relocation in section.iter_relocations():
+                if relocation["r_info_sym"] == 0:
+                    continue 
+                relocation_data = {}
+                relocation_data["offset"] = relocation["r_offset"]
+                relocation_data["info"] = relocation["r_info"]
+                relocation_data["info_type"] = describe_reloc_type(relocation["r_info_type"], elf)
+                relocation_data["symbol"] = relocation["r_info_sym"] 
+                symbol = symbols.get_symbol(relocation_data["symbol"])
+                if symbol["st_name"] == 0:
+                    symbol_section = elf.get_section(symbol["st_shndx"])
+                    relocation_data["symbol_name"] = symbol_section.name
+                else: 
+                    relocation_data["symbol_name"] = symbol.name 
+                
+                relocation_data["symbol_value"] = symbol["st_value"]
+
+                relocations.append(relocation_data)
+    return relocations 
+
+def get_sections(elf_filename):
+    sections = {}  
+    with open(elf_filename, "rb") as elf_file:
+        elf = ELFFile(elf_filename)
+        for section in elf.iter_sections():
+            section_data = {}
+            section_data["name"] = section.name 
+            section_data["address"] = section["sh_addr"]
+            section_data["type"] = section["sh_type"]
+            section_data["flags"] = section["sh_flags"]
+            section_data["offset"] = section["sh_offset"]
+            section_data["size"] = section["sh_size"]
+            section_data["link"] = section["sh_link"]
+            section_data["info"] = section["sh_info"]
+            section_data["addralign"] = section["sh_addralign"]
+            section_data["size"] = section["sh_entsize"]
+            section_data["data"] = section.data()
+            sections[section.name] = section_data 
+
+    return sections
+        
+
+parser = argparse.ArgumentParser(description = "Relocable modules and shared libraries generator")
+parser.add_argument("-i", "--input", dest="elf_filename", action="store", help="Path to input file")
+parser.add_argument("-o", "--output", dest="output_directory", action="store", help="Path to output file")
+args, rest = parser.parse_known_args()
+
+print("Filename: ", args.elf_filename)
+
+symbols = get_symbols(args.elf_filename)
+print(get_public_functions_from_symbols(symbols))
+relocations = get_relocations(args.elf_filename)
+print(relocations)
---
+++ src/modules/example/main.cpp
@@ -0,0 +1,8 @@
+
+void write_to_usart(const char* data);
+
+int object_main()
+{
+    write_to_usart("Hello from module");
+    return 0;
+}
