25505d6086108f6742303b6299e6b78b239c1611
Mateusz Stadnik <matgla@live.com>
2020-09-14 22:48:24 +0200

    added custom language in KConfig to modify parameters automatically

--- cmake/GenerateConfig.cmake
+++ cmake/GenerateConfig.cmake
@@ -6,14 +6,24 @@ function (generate_config)
         COMMAND ${CMAKE_COMMAND} -E env
             srctree=${PROJECT_SOURCE_DIR}
             BOARD_DIR=${PROJECT_SOURCE_DIR}/boards/**
-            ${GENERATE_CONFIG} --input ${PROJECT_BINARY_DIR}/.config --output ${PROJECT_BINARY_DIR}/config.cmake --kconfig ${PROJECT_SOURCE_DIR}/Kconfig
+            ${GENERATE_CONFIG} --input ${PROJECT_BINARY_DIR}/.config --output ${PROJECT_BINARY_DIR} --kconfig ${PROJECT_SOURCE_DIR}/Kconfig
+        RESULT_VARIABLE rc
     )
 
+    if (${rc})
+        message (FATAL_ERROR "Generation of config from KConfig failed")
+    endif ()
+
+    set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${GENERATE_CONFIG})
+    set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${PROJECT_BINARY_DIR}/.config)
     set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${PROJECT_BINARY_DIR}/config.cmake)
+    set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${PROJECT_BINARY_DIR}/config.json)
     include (${PROJECT_BINARY_DIR}/config.cmake)
 
     set_source_files_properties(${PROJECT_BINARY_DIR}/config.cmake PROPERTIES
         GENERATED TRUE
     )
+
+    set (hal_user_config_file "${PROJECT_BINARY_DIR}/config.json" CACHE INTERNAL "")
 
 endfunction()
--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -18,21 +18,102 @@
 
 import argparse
 import sys
+import os
+import json
 
 from kconfig.kconfiglib import Kconfig, Symbol, Choice, MENU, COMMENT
 
 parser = argparse.ArgumentParser(description = "CMake configuration generator based on KConfig")
 parser.add_argument("-i", "--input", dest="input_file", action="store", help="Path to input file", required=True)
-parser.add_argument("-o", "--output", dest="output_file", action="store", help="Path to output file", required=True)
+parser.add_argument("-o", "--output", dest="output_directory", action="store", help="Path to output directory", required=True)
 parser.add_argument("-k", "--kconfig", dest="kconfig", action="store", help="Path to kconfig file", required=True)
 
 args, rest = parser.parse_known_args()
 
+to_parse = []
+
+def process_symbol(symbol, value):
+    print("s: ", symbol)
+    obj = {}
+    current_obj = obj
+    for part in symbol:
+        if part.startswith("KEY$"):
+            print ("this is key")
+        elif part.endswith("$ARRAY"):
+            part = part.replace("$ARRAY", "")
+            current_obj[part.lower()] = []
+            current_obj = current_obj[part.lower()]
+        elif part.find("$") != -1:
+            print("part: ", part)
+            part = part.split('$')
+            part = [i for i in part if i]
+            key = part[0].lower()
+            val = None
+            if len(part) == 1:
+                val = value.lower()
+            else:
+                val = part[1].lower()
+
+            print(current_obj)
+            if isinstance(current_obj, dict):
+                current_obj.update({key: val})
+            else:
+                current_obj.append({key: val})
+                current_obj = current_obj[-1]
+                print("c: ", current_obj)
+
+        else:
+            if isinstance(current_obj, dict):
+                current_obj[part.lower()] = {}
+                current_obj = current_obj[part.lower()]
+            else:
+                current_obj.append({})
+
+    return obj
+
+
+def merge(old, new):
+    for k in new:
+        if k in old:
+            if isinstance(old[k], dict):
+                merge(old[k], new[k])
+            if isinstance(old[k], list):
+                old[k] = old[k] + new[k]
+            else:
+                old[k] = new[k]
+        else:
+            old[k] = new[k]
+
+    return old
+
+
+def generate_json():
+    config = {}
+    for entity in to_parse:
+        # print ("entity: ", entity)
+        splitted_name = entity["name"].split('.')
+        if splitted_name[0] == "PARAM":
+            pass
+        if splitted_name[0] == "PARAM_ARRAY":
+            print(entity)
+            obj = process_symbol(splitted_name[1:], entity["value"])
+            merge(config, obj)
+
+    return config
+
 def main():
     kconf = Kconfig(args.kconfig)
     kconf.load_config(args.input_file)
-    with open(args.output_file, "w") as output:
+    if not os.path.exists(args.output_directory):
+        os.makedirs(args.output_directory)
+
+    with open(args.output_directory + "/config.cmake", "w") as output:
         for node in kconf.unique_defined_syms:
             if node.user_value:
+                to_parse.append({"name": node.name, "value": node.user_value})
                 output.write("set(" + node.name + " " + str(node.user_value) + " CACHE INTERNAL \"\")\n")
+
+    config = generate_json()
+    with open(args.output_directory + "/config.json", "w") as output:
+        output.write(json.dumps(config))
 main()
